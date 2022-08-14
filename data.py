7c5db6edce4faa03a8fc7efce75182e1cb770715
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2022-08-14 17:24:36 +0600

    Well.... Yeah

--- data.py
+++ data.py
@@ -1,11 +1,28 @@
 import network
+import time
+import urequests
 
 wifi = network.WLAN(network.STA_IF)
 wifi.active(True)
 
-networks=wifi.scan()
+networks = wifi.scan()
 print(networks)
 
-wifi.connect('HP_LaserZet_2045', 'fahimfahim')
-print(wifi.isconnected())
+CONNECTION_TIMEOUT_SEC = 10
 
+wifi.connect('HP_LaserZet_2045', 'fahimfahim')
+
+print("connecting...")
+while not wifi.isconnected() and CONNECTION_TIMEOUT_SEC < 0:
+    print(10 - CONNECTION_TIMEOUT_SEC)
+    CONNECTION_TIMEOUT_SEC -= 1
+    time.sleep(1000)
+
+if wifi.isconnected():
+    data = urequests.get("https://jsonbase.com/demo_bucket/hello")
+    print(data.text)
+    print("connected")
+else:
+    print("failed")
+
+print(wifi.status())
